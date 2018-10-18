efac982231ed8efb7857553c730aa2dde5fa9565
Fabian Arancibia <fabian.arancibia93@gmail.com>
2018-10-18 01:08:24 -0300

    Proyecto Web Basico Terminado

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, verbose_name = 'Titulo')
+    description = models.TextField(verbose_name = 'Descripcion')
+    image = models.ImageField(verbose_name = 'Imagen',upload_to="projects" )
+    link = models.URLField(verbose_name = 'Direccion Web', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True,verbose_name = 'Fecha de creacion')
+    updated = models.DateTimeField(auto_now=True,verbose_name = 'Fecha de actualizacion')
+    
 
     class Meta:
         verbose_name = 'Proyecto'
